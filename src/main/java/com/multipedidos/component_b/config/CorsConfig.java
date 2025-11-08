109386ce686761d34942bd839bfc920150f6d0c4
Luis Angel Yuman Rodas <lyumanr@miumg.edu.gt>
2025-11-08 00:26:52 -0600

    Comunicado con el maven compartido

---
+++ src/main/java/com/multipedidos/component_b/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.multipedidos.component_b.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000", // ⚠️ tu frontend
+                                "http://localhost:8081", // ⚠️ tu frontend
+                                "https://nextjs-dashboard-zeta-two-24.vercel.app/" // si luego lo despliegas
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
